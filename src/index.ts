d079ba7c2ce8f26111a82b38fcf2e4ff60b4ed4e
Raghav Misra <raghav.m2014@gmail.com>
2021-07-08 22:55:04 -0700

    Write the install function for the plugin

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import { PluginObject } from "vue";
 
+import bind from "./hooks/bind";
+import update from "./hooks/update";
+import unbind from "./hooks/unbind";
+
 export interface IVueStylistConfig {
     directive?: string;
 }
 
 const VueStylist: PluginObject<IVueStylistConfig> = {
     install(Vue, { directive }) {
+        Vue.directive(directive || "css", {
+            bind,
+            update,
+            componentUpdated: update,
+            unbind,
+        });
     }
 }
 
