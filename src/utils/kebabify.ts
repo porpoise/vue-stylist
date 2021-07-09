2f0caf319ad222bc209f34404722d1c06a1f3969
Raghav Misra <raghav.m2014@gmail.com>
2021-07-09 00:24:17 -0700

    Rename util function to kebabify

--- src/utils/kebabify.ts
+++ src/utils/kebabify.ts
@@ -1,4 +1,4 @@
-const camelToKebab = (input: string): string =>
+const kebabify = (input: string): string =>
     input.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, "$1-$2").toLowerCase();
 
-export default camelToKebab;
+export default kebabify;
